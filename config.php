c9e739b45c778edbfab81aca2610729785597d7d
xxxPchelkinxxx <47283056+xxxPchelkinxxx@users.noreply.github.com>
2019-02-03 16:07:33 +0100

    twotzę parametry perwszego produktu

--- config.php
+++ config.php
@@ -1,5 +1,21 @@
 <? php
 abstract class Config {
 
+  const Data = array (
+    // Продукты пчелы
+    'product' => array(
+      array(''), //Первый элемент пустой
+      array(
+        'name' => 'Nalewka z propolisu',
+        'name_negative' => 'nalewki z propolisu',
+        'unit' => array('krapla', 'krapli', 'krapel'), // Еденицы измерения со склонением
+        'vital' => array('butelka', 'butelki', 'butelek'), //Емкость со склонением
+        'image' => '', //Изображение продукта
+        'age' => array( //Норма употреления исходя из возраста
+          array(''),
+          array('name' => '3-6 lat', 'n' => 5),
+          array('name' => '7-10 lat', 'n' => 10),
+      
+  
 }
 ?>
