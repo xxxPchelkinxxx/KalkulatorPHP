4a53e75094f56628394ff3ca4c47f55a3cc95d99
xxxPchelkinxxx <47283056+xxxPchelkinxxx@users.noreply.github.com>
2019-02-07 14:41:55 +0100

    stwoczyłem konstruktor i metody

--- calculate_class.php
+++ calculate_class.php
@@ -9,6 +9,27 @@ private $product; // приватная Переменная для продкт
 	private $item; // приватная Переменная, которая в index.php везде используется
 	private $n; // Сколько принимать капель/грамм/штук
   
+	//Конструктор, сюда передаем данные пользователя
+	public function_construct($product, $age, $times, $period, $item)
+				  { $this->item = $item; // Инициализируем переменную item
+				   $this-> insertValue($this->intDo($product), $this->intDo($age), $this->intDo($times), $this->intDo(%period); //вызываем метод, в котором получаем данные из конфига
+				  }	
+	//Метод, который преобразует переменную в тип int
+private function intDo($data)
+						       {
+							       return (int) $data;
+						       }
+       // метод Сколько принимать капель/грам/шт
+private function countG()
+						       {
+						       foreach ($this->product['age'] as $this->item){
+							       if ($this->item['name'] ==$this->age['name']){
+							       $this->n = (float) $this->item['n'];
+								       breack;
+							       }
+						       } 
+						       }
+						       
   
   
   }
