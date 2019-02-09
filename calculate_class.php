9dcc9404f76b3387a7a1300459e89dcd030e62b2
xxxPchelkinxxx <47283056+xxxPchelkinxxx@users.noreply.github.com>
2019-02-09 21:15:32 +0100

    Stwórzylem metody, które ilości opakuwań, metodę która wyzywa konkratnie czysło, metodę która otrzymuje dane z klasu Config

--- calculate_class.php
+++ calculate_class.php
@@ -30,7 +30,53 @@ private function countG()
 						       } 
 						       }
 						       
-  
+  // Метод количество емкостей
+	private function containers()
+						       {
+						       $this->countG(); //Вызов метода countG
+							      return ceil($this->n*$this->period['n']*$this->times['n']/$this->product['volume']);
+							      						       }
+						       // Метод который формирует массив и возвращает его
+						       public function getResult()
+						       {
+							       $number_vial = $this->containers(); // Вызов метода, чтоб получить значение
+							       return array(
+								       'image'		=> $this->product['image'], //Изображение
+								       'product'	=> $this->product['name'], // Название продукта
+								       'age'		=> $this->age['name'], // Возраст
+								       'n'		=>$this->n, // Сколько принимать капель, граммб штук
+								       'times'		=>$this->times['name'], // Сколько раз принимат в день
+								       'period'		=>$this->period['n'], // Период приема
+								       'number_vial'	=>$number_vial, // Количество емкостей
+								       'declination_unit'	=>$this->declination($this->n, $this->product['unit']), // Склонение единицы измерения
+								       'declination_period'	=>$this->declination($this->period['n'], array('dzień','dniu','dni')), // Склонение периода
+								       'declination_vial'	=>$this->declination($number_vial, $this->product['vial']),	 //	Склонение емкости		     
+						       );
+								       }
+						       // Метод склонения существительных с числительными
+						       private function declination ($n, $form) 
+						       {
+						       $n = abs($n)%100;
+						       $n1 = $n%10;
+							       
+							       if ($n > 10 && $n < 20) return $form[2];
+							       if ($n > 1 && $n1 < 5) return $form[1];
+							       if ($n > 0 && $n1 <= 1) return $form[0];
+							       
+							       return $form[2];
+						       }
+						       //Метод который получает данные из массива, которые находятся в классе config
+						       private function insertValue ($product, $age, $times, $period) 
+						       {
+						       		$this->product = Config::DATA['product'][$product];
+								$this->age     = Config::DATA['product'][$product]['age'][$age];
+								$this->times   = Config::DATA['times'][$times];
+								$this->period  = Config::DATA['period'][$period];
+						       }
+						       
+			
+						       }
+						 ?>
   
   }
 
